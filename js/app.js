d96e131facc2f5b69e98ddcb611de97f122af6d0
brittonhayes <46035482+brittonhayes@users.noreply.github.com>
2019-01-07 21:08:54 -0800

    Added scroll to top

--- js/app.js
+++ js/app.js
@@ -33,11 +33,10 @@ $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
@@ -49,7 +48,7 @@ $('a[href*="#"]')
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000, function() {
+        }, 1000, function () {
           // Callback after animation
           // Must change focus!
           var $target = $(target);
@@ -57,10 +56,21 @@ $('a[href*="#"]')
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
           };
         });
       }
+    }
+  });
+
+
+  // scroll to top show
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 600) {
+      $('#scrollTop').fadeIn(400);
+    } else {
+      $('#scrollTop').fadeOut(400);
     }
   });
